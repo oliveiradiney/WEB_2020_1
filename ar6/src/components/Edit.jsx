e9c5b3a6d412ce6d481f0022f7f2bfb405c95356
oliveiradiney <dineyoliveira1999@gmail.com>
2020-08-31 18:28:41 -0300

    Atividade 6(Que usa o servidor express da atividade 5)

---
+++ ar6/src/components/Edit.jsx
@@ -0,0 +1,76 @@
+import  React, { Component } from 'react'
+import axios from 'axios'
+export default class Edit extends Component{
+    constructor(props){
+        super(props)
+        this.state = {nome:'', curso:'', capacidade:''} 
+
+        this.setNome =  this.setNome.bind(this)
+        this.setCurso = this.setCurso.bind(this)
+        this.setCapacidade = this.setCapacidade.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+      }
+
+    componentDidMount(){
+       axios.get('http://localhost:3002/disciplinas/retrieve/'+this.props.match.params.id)
+       .then((res)=>{
+            this.setState({
+                nome:res.data.nome,
+                curso:res.data.curso,
+                capacidade:res.data.capacidade
+            })
+       })
+       .catch(error=>console.log(error))
+    }
+    
+    setNome(d){
+        this.setState({nome:d.target.value})
+    }
+    setCurso(d){
+        this.setState({curso:d.target.value})
+    }
+    setCapacidade(d){
+        this.setState({capacidade:d.target.value})
+    }
+
+    onSubmit(d){
+        d.preventDefault()
+        
+        const disciplinaEditada = {nome:this.state.nome, curso:this.state.curso, capacidade:this.state.capacidade}
+        axios.put('http://localhost:3002/disciplinas/update/'+this.props.match.params.id,disciplinaEditada)
+        //axios.put('http://localhost:3002/disciplinas/'+this.props.match.params.id,disciplinaEditada)
+        .then(
+            (res)=>{
+                this.props.history.push('/list')
+            }
+        )
+        .catch(error=>console.log(error))
+    }
+
+    render(){
+        return(
+            <div style={{marginTop:10}}>
+                <h3>Editar Disciplina</h3>
+                <form onSubmit={this.onSubmit}>
+                <div className='form-row'>
+                    <div className='form-group col-md-6'>
+                        <label>Nome: </label>
+                        <input type='text' className='form-control' placeholder='Disciplina' value={this.state.nome} onChange={this.setNome}/>
+                    </div>
+                    <div className='form-group col-md-6'>
+                        <label>Curso: </label>
+                        <input type='text' className='form-control' placeholder='Curso' value={this.state.curso} onChange={this.setCurso}/>
+                    </div>
+                    <div className='form-group col-md-12'>
+                        <label>Capacidade: </label>
+                        <input type='text' className='form-control' placeholder='Capacidade' value={this.state.capacidade} onChange={this.setCapacidade}/>
+                    </div>
+                    <div className='form-group col'>
+                        <input type='submit' value='Editar disciplina' className='btn btn-info' />
+                    </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
---
+++ ar6/src/components/List.jsx
@@ -0,0 +1,65 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+import TableRow from './TableRow'
+
+export default class List extends Component{
+    constructor(props){
+        super(props)
+        this.state = {disciplinas:[]}
+        this.apagarElementoId  = this.apagarElementoId.bind(this)
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3002/disciplinas/list')
+        .then(
+            (res)=>{
+                this.setState({disciplinas:res.data})
+            }
+        )
+        .catch(
+            (error)=>{
+                console.log(error)
+            }
+        )
+    }
+    
+    MountTable(){
+        if(!this.state.disciplinas) return
+        return this.state.disciplinas.map(
+            (disciplina, i)=>{
+                return <TableRow disciplinas={disciplina} key={i} apagarElementoId={this.apagarElementoId}/>
+            }
+        )
+    }
+    apagarElementoId(id){
+        let tempDisciplinas = this.state.disciplinas
+        for(let i=0; i < tempDisciplinas.length; i++){
+            if(tempDisciplinas[i]._id === id){
+                tempDisciplinas.splice(i,1)
+            }
+        }
+        this.setState({disciplinas:tempDisciplinas})
+    }
+
+    render(){
+        return(
+            <div style={{marginTop:10}}>
+                <h3>Listar Disciplinas</h3>
+                <table className='table table-hover' style={{marginTop:20}}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Curso</th>
+                            <th>Capacidade</th>
+                            <th colSpan='2'></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            {this.MountTable()}
+                    </tbody>
+                </table>
+            </div>
+        );
+;    }
+}
